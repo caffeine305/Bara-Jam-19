0448eaf8f11adce2e030c8f0eadf50fb11dcaebb
Ricardo Lázaro <caffeine305@gmail.com>
2019-10-26 13:42:11 -0500

    Re-wired animations

--- Assets/Scripts/movimiento.cs
+++ Assets/Scripts/movimiento.cs
@@ -3,17 +3,22 @@ using System.Collections;
 
 public class movimiento : MonoBehaviour
 {
+    private const double V = 0.001;
     CharacterController characterController;
 
     public float speed = 6.0f;
     public float jumpSpeed = 8.0f;
     public float gravity = 20.0f;
+    public float thereshold = 0.0001f;
+
+    public Animator Animar;
 
     private Vector3 moveDirection = Vector3.zero;
 
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        Animar = GetComponent<Animator>();
     }
 
     void Update()
@@ -26,10 +31,20 @@ public class movimiento : MonoBehaviour
             moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
             moveDirection *= speed;
 
+            if( moveDirection.magnitude > thereshold )
+                {
+                Animar.SetBool("IsWalking", true);
+                }else Animar.SetBool("IsWalking", false);
+
             if (Input.GetButton("Jump"))
             {
                 moveDirection.y = jumpSpeed;
             }
+
+            if (Input.GetButton("Jump"))
+            {
+                Animar.SetBool("IsPunching", true);
+            }else Animar.SetBool("IsPunching", false);
         }
 
         // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
